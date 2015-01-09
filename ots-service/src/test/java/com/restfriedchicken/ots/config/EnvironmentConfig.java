191b8d1f7bce9cca48b1022af409ecb9a466a1c3
hippoom <hippoom@gmail.com>
2015-01-09 16:19:00 +0800

    1. Now test source set also supports provided dependencies. 2. Add upload archives task to build.

--- ots-service/src/test/java/com/restfriedchicken/ots/config/EnvironmentConfig.java
+++ ots-service/src/test/java/com/restfriedchicken/ots/config/EnvironmentConfig.java
@@ -1,24 +1,11 @@
 package com.restfriedchicken.ots.config;
 
+import lombok.Data;
+
+@Data
 public class EnvironmentConfig {
 
     private String baseUri;
+
     private int port = 8080;
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-    public String getBaseUri() {
-
-        return baseUri;
-    }
-
-    public void setBaseUri(String baseUri) {
-        this.baseUri = baseUri;
-    }
 }
