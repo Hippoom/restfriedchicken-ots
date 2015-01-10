6c30126d9924fce9d07f76e8d1c5a095e059b97e
hippoom <hippoom@gmail.com>
2015-01-10 14:43:37 +0800

    download ots-service binary with the help from go-maven-poller.

--- fetch-ots-service-binary.sh
+++ fetch-ots-service-binary.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 
 outputfile="ots-service/build/libs/$GO_PACKAGE_LOCAL_NEXUS_OTS_SERVICE_BINARY_ARTIFACT_ID-$GO_PACKAGE_LOCAL_NEXUS_OTS_SERVICE_BINARY_VERSION.$GO_PACKAGE_LOCAL_NEXUS_OTS_SERVICE_BINARY_PACKAGING"
+url=$GO_PACKAGE_REPONAME_PKGNAME_LOCATION
 
 echo $outputfile
+echo $url
 
-curl -o  $outputfile $GO_PACKAGE_REPONAME_PKGNAME_LOCATION
+mkdir -p ots-service/build/libs
+curl -o  $outputfile $url
 
