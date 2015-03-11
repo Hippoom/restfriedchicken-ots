d95e30325a5044286186615528053f422a8508f9
hippoom <hippoom@gmail.com>
2015-03-11 12:28:08 +0800

    Fix cap deploy restart error caused by failed to cat server.pid

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,9 @@ set :rails_env, :production
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+#set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+#set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -43,8 +45,10 @@ set :rails_env, :production
 desc 'Restart application'
 task :restart do
   on roles(:app), in: :sequence, wait: 5 do
-    execute "rails s -e production"  ## -> line you should add
+    server_pid_path = "#{release_path}/tmp/pids/server.pid"
+    execute "if [ -f #{server_pid_path} ]; then kill -SIGTERM `cat #{server_pid_path}`; fi"
+    execute "cd #{release_path} && bundle exec rails s -b 0.0.0.0 -e production -d &"
   end
 end
 
-#after :publishing, :restart
+after :deploy, :restart
--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,11 +1,11 @@
-server_ip = ENV["SERVER_IP"] || "127.0.0.1"
+server_ip = ENV["SERVER_IP"] || "192.168.70.2"
 gem_source = ENV["GEM_SOURCE"] || "https://ruby.taobao.org"
 
 set :default_env, { gem_source: gem_source }
-set :deploy_to, "/Users/twer/Workspace/ots/"
+
+set :deploy_to, "/home/vagrant/ots/"
 set :bundle_flags, '--deployment --verbose' # default is '--deployment --quiet'
 
-set :repo_url, 'file:///Users/twer/Workspace/restfriedchicken-ots/.git/'
-set :branch, 'rails'
+#set :repo_url, 'file:///Users/twer/Workspace/restfriedchicken-ots/.git/'
 
-server server_ip, user: 'twer', roles: %w{web app db}
+server server_ip, user: 'vagrant', roles: %w{web app db}
